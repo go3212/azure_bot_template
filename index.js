a3d37e3285b979f8ec9e40bcae631baf1c5cfbb8
go3212 <go3212@hotmail.com>
2020-11-11 00:28:30 +0100

    Se ha implementado un chat basico.

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-
-var app = require('express');
+const uuid = require('uuid');
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+let randomColor = require('randomcolor');
 
 //app.use(express.static("public"));
 
 var server = http.createServer((request, response) =>
 {   
-    
+
+    //if (request.headers.accept.split(',')[0] == '')
+    //console.log(request.headers);
+    //console.log(pathname);
+
+    var pathname = url.parse(request.url).pathname;
+
+    if (pathname == '/chat.js' || pathname == '/modalScript.js') 
+    {
+        var file = fs.readFileSync("./public" + pathname);
+        response.write(file);
+        response.end()
+    }
+
     if(request.headers.accept.split(',')[0] == 'text/css')
     {
         //console.log(request.headers);
@@ -20,10 +34,10 @@ var server = http.createServer((request, response) =>
     } 
     else 
     {
-        fs.readFile('./public/index.htm', (error, html) =>
+        fs.readFile('./public/index.html', (error, html) =>
         {
             if (error) throw error;
-
+            
             response.writeHeader(200, { 'Content-Type': 'text/html' });
             response.write(html);
             response.end();
@@ -31,7 +45,63 @@ var server = http.createServer((request, response) =>
     }
 });
 
-server.listen(8080, 'localhost', 200, () =>
+server.listen(3000, '172.20.152.181', () =>
 {
     console.log('Server started');
 });
+
+const io = require("socket.io")(server);
+
+let users = [];
+let connections = [];
+
+io.on('connection', (socket) =>
+{
+    console.log('New user connected');
+    connections.push(socket);
+    
+    let color = randomColor();
+
+    socket.username = 'Anonymous';
+    socket.color = color;
+
+    socket.on('change_username', data =>
+    {
+        let id = uuid.v4();
+        socket.id = id;
+        socket.username = data.nickName;
+        users.push({ id, username: socket.username, color: socket.color });
+        updateUsernames();
+    });
+    
+    const updateUsernames = () =>
+    {
+        io.sockets.emit('get users', users);
+    };
+
+    socket.on('new_message', (data) =>
+    {
+        io.sockets.emit('new_message', { message: data.message, username: socket.username, color: socket.color });
+    });
+
+    socket.on('disconmect', data =>
+    {
+        if (!socket.username) return;
+
+        let user = undefined;
+        for (let i = 0; i < users.length; ++i)
+        {
+            if (users[i].id == socket.id)
+            {
+                user = users[i];
+                break;
+            }
+        }
+
+        users.splice(user, 1);
+
+        updateUsernames();
+        connections.splice(connections.indexOf(socket, 1));
+    });
+});
+

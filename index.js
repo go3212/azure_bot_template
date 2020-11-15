b7242e2d113f32edf4a5865df399dd30550f8923
go3212 <go3212@hotmail.com>
2020-11-15 16:29:34 +0100

    Base de datos de conversacion y mejora en interfaz

--- index.js
+++ index.js
@@ -58,39 +58,58 @@ server.listen(4000, '192.168.1.60', () =>
 //  LÓGICA DE LA BASE DE DATOS  //
 //////////////////////////////////
 
-/* Lógica de la base de datos
-var database = fs.createReadStream(__dirname + '/database/users.json', 'utf8');
+// Lógica de la base de datos, se crea un fichero de conversaciones diario.
+var file_extension = '.csv';
+var date = ((((new Date()) + '').split(' ').join('_')).split('_GMT', 1)[0]).slice(0,-9);
+var file_directory = __dirname + '/database/' + date + file_extension;
 
-database.on('data', (chunk) => 
+fs.writeFile (file_directory, '', { flag: 'wx' }, (err) =>
 {
-    console.log(chunk);
+    if (err) console.log(date + file_extension + " file exists");
 });
-*/
+
 
 /////////////////////////////////////////////////////////
 //  EVENTOS INPUT-OUTPUT, INTERACCÓN CLIENTE-SERVIDOR  //
 /////////////////////////////////////////////////////////
 
-const io = require("socket.io")(server);
+const io = require ("socket.io")(server);
 
 let users = [];
 let connections = [];
 let user_addresses = {};
 
-io.on('connection', (socket) =>
+io.on ('connection', (socket) =>
 {
-    console.log('Nueva conexión');
+    console.log('Nueva conexión: ' + socket.handshake.address);
     connections.push(socket);
     
     // Esta función actualiza los nombres de usuraio en los clientes (WIP)
     const updateUsernames = () =>
     {
         return new Promise (resolve =>
-            {
-                setTimeout(() => resolve(io.sockets.emit('get users', users)), 0);
-            });
-        };
-        
+        {
+            setTimeout(() => resolve(io.sockets.emit('get users', users)), 0);
+        });
+    };
+    
+    /////////////////////////////////////
+    //   ENVIAR CHAT PREVIO A USUARIO  //
+    /////////////////////////////////////
+    let database = fs.createReadStream(file_directory, 'utf8');
+    database.on('data', (chunk) => 
+    {
+        // Básicamente, el archivo separa las lineas con saltos de linea, los cortamos. Además, siempre hay una linea entera sin elementos. La eliminamos.
+        chunk = chunk.split('\n');
+        chunk.pop();
+        for (item in chunk)
+        {
+            item = chunk[item].split(',');
+            let data = { uuid: item[0], username: item[1], color: item[2], message: item[3] };
+            socket.emit('chat-setup', data);
+        }
+    });
+    
     /////////////////////////////////////
     //  LÓGICA DE GESTIÓN DE USUARIOS  //
     /////////////////////////////////////
@@ -103,12 +122,14 @@ io.on('connection', (socket) =>
     {
         socket.username = "Anonymous";
         socket.color = randomColor();
+        socket.id = uuid.v4();
+
+        user_addresses[user_ip] = { uuid: socket.id, username: "Anonymous", color: socket.color, logged: false };
         
-        user_addresses[user_ip] = { uuid: uuid.v4(), username: "Anonymous", color: socket.color, logged: false };
-        
-        socket.on('change_username', data =>
+        socket.on ('change_username', data =>
         {
             socket.username = data.nickName;
+            socket.color = user_addresses[user_ip].color;
             user_addresses[user_ip].username = data.nickName;
             user_addresses[user_ip].logged = true;
             users.push(user_addresses[user_ip].username);
@@ -122,16 +143,14 @@ io.on('connection', (socket) =>
         socket.id = user.uuid;
         socket.color = user.color;
         users.push(user_addresses[user_ip].username);
-        console.log(users);
         socket.emit('logged');
     }
     
     updateUsernames();
     
     // Se detecta la desconexión de algun usuario
-    socket.on('disconnect', data =>
+    socket.on ('disconnect', data =>
     {
-
         if (users.includes(socket.username)) 
         {
             users.splice(users.indexOf(socket.username), 1);
@@ -145,13 +164,20 @@ io.on('connection', (socket) =>
     ////////////////////////////////////////////
 
     // Se remite a todos los clientes la información de los mensajes entrantes.
-    socket.on('new_message', (data) =>
+    socket.on ('new_message', (data) =>
     {
+        // Guardar las conversaciones en un archivo.
+        let information = socket.id + ',' + socket.username + ',' + socket.color + ',' + data.message;
+        fs.appendFile(file_directory, information + '\n', (err) =>
+        {
+            if (err) throw err;
+        });
+
         io.sockets.emit('new_message', { message: data.message, username: socket.username, color: socket.color });
     });
 
-    // Se remite se algún usuario escribe.
-    socket.on('typing', data =>
+    // Se remite si algún usuario escribe.
+    socket.on ('typing', data =>
     {
         socket.broadcast.emit('typing', { username: socket.username })
     });
